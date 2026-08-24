66042cc8d6b49c028eb18e1cf2790116d1195fb3
Albert <albert.school2015@gmail.com>
2026-08-24 11:20:47 +0300

    3rd commit

---
+++ app/src/main/java/com/example/test260824/FBRef.java
@@ -0,0 +1,7 @@
+package com.example.test260824;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FBRef {
+    public static FirebaseAuth refAuth = FirebaseAuth.getInstance();
+}
--- app/src/main/java/com/example/test260824/MainActivity.java
+++ app/src/main/java/com/example/test260824/MainActivity.java
@@ -1,12 +1,27 @@
 package com.example.test260824;
 
+import static com.example.test260824.FBRef.refAuth;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createUser("a@a.com", "123456");
+
+    }
+    private void createUser(String email, String pass) {
+        refAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.i("MainActivity", "createUserWithEmailAndPassword:success");
+                            FirebaseUser user = refAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "User created successfully\nUid: "+user.getUid(), Toast.LENGTH_LONG).show();
+                        } else {
+                            Exception exp = task.getException();
+                            if (exp instanceof FirebaseAuthInvalidUserException){
+                                Toast.makeText(MainActivity.this, "Invalid email address.", Toast.LENGTH_SHORT).show();
+                            } else if (exp instanceof FirebaseAuthWeakPasswordException) {
+                                Toast.makeText(MainActivity.this, "Password too weak.", Toast.LENGTH_SHORT).show();
+                            } else if (exp instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(MainActivity.this, "User already exists.", Toast.LENGTH_SHORT).show();
+                            } else if (exp instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(MainActivity.this, "General authentication failure.", Toast.LENGTH_SHORT).show();
+                            } else if (exp instanceof FirebaseNetworkException) {
+                                Toast.makeText(MainActivity.this, "Network error. Please check your connection.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "An error occurred. Please try again later.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
     }
 }
